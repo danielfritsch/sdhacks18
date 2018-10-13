d16802ec3c12a39ec85ca711af40199cd3aced66
Michael Chen <michael.chen880@gmail.com>
2018-10-12 21:50:46 -0700

    configuring front end stuff...webpack and babel configs etc. nothing works yet

---
+++ static/webpack.config.js
@@ -0,0 +1,14 @@
+const webpack = require('webpack');
+
+const config = {
+    entry:  __dirname + '/js/index.js',
+    output: {
+        path: __dirname + '/dist',
+        filename: 'bundle.js',
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.css']
+    },
+};
+
+module.exports = config;
